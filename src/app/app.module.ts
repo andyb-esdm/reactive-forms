bf4842e63d136893f13e0b43f55e8541a0574da7
andyb <ambrewer69@gmail.com>
2021-11-17 19:21:18 +0000

    select example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectExampleComponent } from './select-example/select-example.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SelectExampleComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/select-example/select-example.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface Option {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-select-example',
+  templateUrl: './select-example.component.html',
+  styleUrls: ['./select-example.component.css'],
+})
+export class SelectExampleComponent {
+  options: Option[] = [
+    { id: 1, name: 'Banjo' },
+    { id: 2, name: 'Advocado' },
+    { id: 3, name: 'Uncle Barry' },
+    { id: 4, name: 'Lettuce' },
+  ];
+
+  nameForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.nameForm = this.fb.group({
+      name: [null, Validators.required],
+    });
+  }
+}
